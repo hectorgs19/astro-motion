ce35252e98cb77fa2fd8ae767744ab14871628f6
Hector Gomez <hectorgomezsoria@gmail.com>
2024-12-18 21:53:05 +0100

    AM-1002-dv: Clases en json

---
+++ src/data/clases.js
@@ -0,0 +1,11 @@
+//Opción 2 para cargar datos desde JS.
+
+export const clases = [
+    { day: "Lunes", startHour: 9, duration: 2, level: "Principiante", participants: Math.floor(Math.random() * 8) + 1 },
+    { day: "Lunes", startHour: 14, duration: 1, level: "Intermedio", participants: Math.floor(Math.random() * 8) + 1 },
+    { day: "Martes", startHour: 11, duration: 1.5, level: "Avanzado", participants: Math.floor(Math.random() * 8) + 1 },
+    { day: "Miércoles", startHour: 14, duration: 1.5, level: "Intermedio", participants: Math.floor(Math.random() * 8) + 1 },
+    { day: "Jueves", startHour: 10, duration: 2, level: "Principiante", participants: Math.floor(Math.random() * 8) + 1 },
+    { day: "Viernes", startHour: 12, duration: 3, level: "Avanzado", participants: Math.floor(Math.random() * 8) + 1 },
+    { day: "Sábado", startHour: 8, duration: 1, level: "Principiante", participants: Math.floor(Math.random() * 8) + 1 },
+  ];
